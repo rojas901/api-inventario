15eff89dbc85b24c3ae0b5f86d71e229ba616d7f
Carlos Rojas <caranroja@outlook.com>
2022-09-01 16:24:00 -0500

    Se instala morgan y se agregan las rutas en el index de la aplicacion

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const morgan = require('morgan');
 const port = 3000;
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => { 
     res.status(200).send('<h1>La API funciona correctamente</h1>');
 });
+
+app.use('/tipos', routes.tiposRoutes);
+app.use('/estados', routes.estadosRoutes);
+app.use('/usuarios', routes.usuariosRoutes);
+app.use('/marcas', routes.marcasRoutes);
+app.use('/inventario', routes.inventarioRoutes);
 
 app.listen(port, ()=>{
     console.log(`Escuchando en el puerto http://localhost:${port}`);
